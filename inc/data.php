6acf81949c0cd376a9b6ff1470efbf24cdc807c5
Doskievinson <39282041+Doskievinson@users.noreply.github.com>
2018-06-04 17:43:49 -0400

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,60 @@
+<?php
+$catalog[101] = [
+    "title" => "Atmospheric Disturbances",
+    "img" => "resources/atmospheric.jpg",
+   "genre" => "Self-help",
+   "format" => "Paperback",
+   "year" => 2016,
+   "category" => "Books",
+   "authors" => [
+       "Desmond Tutu",
+       "14th Dalai Lama",
+       "Douglas Abrams"
+   ],
+   "publisher" => "Penguin Publishing Group",
+   "isbn" => '9780399185045'
+];
+
+*//Movies*
+$catalog[201] = [
+   "title" => "Moana",
+   "img" => "resources/moana.jpg",
+   "genre" => "Action,Adventure,Fantasy",
+   "format" => "DVD",
+   "year" => 2009,
+   "category" => "Movies",
+   "director" => "James Cameron",
+   "writers" => [
+       "James Cameron"
+
+   ],
+   "stars" => [
+       "Sam Worthington",
+       "Zoe Saldana",
+       "Sigourney Weaver"
+   ]
+];
+
+*//Music*
+$catalog[301] = [
+   "title" => "Mister MEllow",
+   "img" => "resources/mister mellow.jpg",
+   "genre" => "Clasical",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Ludwig van Beethoven"
+];
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+
---
+++ inc/footer.php
@@ -0,0 +1,11 @@
+<div class="footer">
+<div class="wrapper">
+<ul>
+	<li><a href="www.twitt">Twitter</a></li>
+	<li><a href="www.faceboo">Facebook</a></li>
+</ul>
+<p>&copy;<?php echo date("Y"); 2018 My Online Library Collection</p>
+</div>
+	</div>
+</body>
+</html>
